f837962f691652cfef38430b6b950e33033b84b8
Mattia Centini <mattia.centini@gmail.com>
2020-10-21 15:29:48 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+            </div>
+            <div className="header__right">
+                <ul>
+                    <li>Workshops</li>
+                    <li>About me</li>
+                    <li>Contact</li>
+
+                </ul>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
